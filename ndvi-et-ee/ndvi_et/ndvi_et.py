3f41b8b89ad04fe739c8c2428695314d7f0901fa
charles morton <charles.morton@dri.edu>
2017-10-26 14:00:27 -0700

    Starting to add a simple NDVI ET model

---
+++ ndvi-et-ee/ndvi_et/ndvi_et.py
@@ -0,0 +1,52 @@
+import ee
+
+ee.Initialize()
+
+
+class NDVI_ET(object):
+    """Earth Engine DisALEXI"""
+
+    def __init__(self, toa_image,
+                 m=1.25, b=0.0,
+                 elevation=ee.Image('USGS/NED')):
+        """Initialize an image for computing NDVI ET
+
+        """
+        self.toa_image = toa_image
+
+        # Use the SPACECRAFT_ID property identify each Landsat type
+        self.spacecraft_id = ee.String(self.toa_image.get('SPACECRAFT_ID'))
+
+        # Rename bands to generic names
+        input_bands = ee.Dictionary({
+            'LANDSAT_5': ['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'B6', 'BQA'],
+            'LANDSAT_7': ['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'B6_VCID_1', 'BQA'],
+            'LANDSAT_8': ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B10', 'BQA']})
+        output_bands = [
+            'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'lst', 'bqa']
+
+        self.image = ee.Image(self.toa_image) \
+            .select(input_bands.get(self.spacecraft_id), output_bands)
+
+    def compute_etf(self):
+        """"""
+        return self._get_ndvi(self.image) \
+            .multiply(self.m).add(self.b) \
+            .rename(['etf'])
+
+    def _get_ndvi(self):
+        """Compute NDVI
+
+        Parameters
+        ----------
+        self.input_image : ee.Image
+
+        Returns
+        -------
+        ndvi : ee.Image
+
+        """
+        return self.input_image.normalizedDifference(['nir', 'red']) \
+            .rename(['ndvi'])
+
+    # def _prep():
