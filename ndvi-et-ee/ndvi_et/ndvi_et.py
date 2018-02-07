1dee92acbffe71e4b8d02492fb1e5f6d5d5230ac
Charles Morton <charles.g.morton@gmail.com>
2018-02-06 16:12:36 -0800

    Playing around with the generic NDVI ET model and class

--- ndvi-et-ee/ndvi_et/ndvi_et.py
+++ ndvi-et-ee/ndvi_et/ndvi_et.py
@@ -4,82 +4,125 @@ ee.Initialize()
 
 
 class NDVI_ET(object):
-    """Earth Engine DisALEXI"""
+    """GEE based model for computing ETf as a linear function of NDVI"""
 
-    def __init__(self, input_image,
-                 m=1.25, b=0.0,
+    def __init__(self, input_image, m=1.25, b=0.0,
                  elevation=ee.Image('USGS/NED')):
-        """Initialize an image for computing NDVI ET
-
-        """
-        self.toa_image = input_image
-
-        # Use the SPACECRAFT_ID property identify each Landsat type
-        self.spacecraft_id = ee.String(self.toa_image.get('SPACECRAFT_ID'))
-
-        # Rename bands to generic names
-        input_bands = ee.Dictionary({
-            'LANDSAT_5': ['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'B6', 'BQA'],
-            'LANDSAT_7': ['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'B6_VCID_1', 'BQA'],
-            'LANDSAT_8': ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B10', 'BQA']})
-        output_bands = [
-            'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'lst', 'bqa']
-
-        self.image = ee.Image(self.toa_image) \
-            .select(input_bands.get(self.spacecraft_id), output_bands)
-
-    def compute_etf(self):
-        """"""
-        return self._get_ndvi(self.image) \
-            .multiply(self.m).add(self.b) \
-            .rename(['etf'])
-
-    def _get_ndvi(self):
-        """Compute NDVI
+        """Create an NDVI_ET object
 
         Parameters
         ----------
-        self.input_image : ee.Image
+        input_image :
+            Must have "ndvi" and "qa" bands
+        m : float
+            Slope.
+        b : float
+            Offset.
+        elevation : ee.Image
+            Elevation image (not currently being used)
+
+        Notes
+        -----
+        ETf = m * NDVI + b
+
+        """
+        # Unpack the inputs
+        self.input_image = input_image
+        self._m = m
+        self._b = b
+        self._elevation = elevation
+
+        # Unpack the input bands?
+        # self.ndvi = ee.Image(input_image).select(['ndvi'])
+
+    def etf(self):
+        """"""
+        return ee.Image(self.input_image).select(['ndvi']) \
+            .multiply(self._m).add(self._b) \
+            .rename(['etf'])
+
+    @classmethod
+    def LandsatTOA(cls, toa_image):
+        """Constructs an NDVI_ET object from a Landsat TOA image
+
+        Parameters
+        ----------
+        toa_image : ee.Image
 
         Returns
         -------
-        ndvi : ee.Image
+        A NDVI_ET
 
         """
-        return self.input_image.normalizedDifference(['nir', 'red']) \
-            .rename(['ndvi'])
-
-
-class Landsat():
-    def __init__()
         # Use the SPACECRAFT_ID property identify each Landsat type
-        self.spacecraft_id = ee.String(self.toa_image.get('SPACECRAFT_ID'))
+        spacecraft_id = ee.String(ee.Image(toa_image).get('SPACECRAFT_ID'))
 
         # Rename bands to generic names
         input_bands = ee.Dictionary({
-            'LANDSAT_5': ['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'B6', 'BQA'],
-            'LANDSAT_7': ['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'B6_VCID_1', 'BQA'],
-            'LANDSAT_8': ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B10', 'BQA'],
+            'LANDSAT_5': ['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'B6'],
+            'LANDSAT_7': ['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'B6_VCID_1'],
+            'LANDSAT_8': ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B10'],
         })
         output_bands = [
-            'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'lst', 'bqa']
+            'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'lst']
+        rename_image = ee.Image(toa_image) \
+            .select(input_bands.get(spacecraft_id), output_bands)
 
-        self.image = ee.Image(self.toa_image) \
-            .select(input_bands.get(self.spacecraft_id), output_bands)
+        # Build the input image
+        input_image = ee.Image([
+            cls._ndvi(rename_image),
+            ee.Image(toa_image).select(['BQA'], ['qa'])])
 
+        # Add properties and instantiate class
+        return cls(ee.Image(input_image).setMulti({
+            'system:time_start': ee.Image(toa_image).get('system:time_start')
+        }))
 
-class Sentinel():
-    def __init__()
-        # Use the SPACECRAFT_NAME property identify each Landsat type
-        self.spacecraft_name = ee.String(self.toa_image.get('SPACECRAFT_NAME'))
+    @classmethod
+    def Sentinel2TOA(cls, toa_image):
+        """Constructs an NDVI_ET object from a Sentinel TOA image
 
+        Parameters
+        ----------
+        toa_image : ee.Image
+
+        Returns
+        -------
+        NDVI_ET
+
+        """
+
+        # Don't distinguish between Sentinel-2 A and B
         # Rename bands to generic names
-        input_bands = ee.Dictionary({
-            'Sentinel-2A': ['B2', 'B3', 'B4', 'B8', 'B7'],
-            'Sentinel-2B': ['B2', 'B3', 'B4', 'B8', 'B7'],
-        })
-        output_bands = [
-            'blue', 'green', 'red', 'nir', 'swir1', 'swir2']
+        # Scale bands to 0-1 (from 0-10000)
+        input_bands = ['B2', 'B3', 'B4', 'B8', 'B11', 'B12']
+        output_bands = ['blue', 'green', 'red', 'nir', 'swir1', 'swir2']
+        rename_image = ee.Image(toa_image) \
+            .select(input_bands, output_bands) \
+            .divide(10000.0)
 
-        self.image = ee.Image(self.toa_image) \
-            .select(input_bands.get(self.spacecraft_id), output_bands)
+        # Build the input image
+        input_image = ee.Image([
+            cls._ndvi(rename_image),
+            ee.Image(toa_image).select(['QA60'], ['qa'])])
+
+        # Add properties and instantiate class
+        return cls(ee.Image(input_image).setMulti({
+            'system:time_start': ee.Image(toa_image).get('system:time_start')
+        }))
+
+    @staticmethod
+    def _ndvi(toa_image):
+        """Compute NDVI from a common TOA image
+
+        Parameters
+        ----------
+        toa_image : ee.Image
+
+        Returns
+        -------
+        ee.Image
+
+        """
+        return ee.Image(toa_image).normalizedDifference(['nir', 'red']) \
+            .rename(['ndvi'])
